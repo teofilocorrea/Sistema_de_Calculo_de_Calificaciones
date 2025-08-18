cbb389687be1b660fbcd653ff5bb5ed5a89de6e0
Teófilo Correa Rojas <teofilocorrea@gmail.com>
2025-08-17 22:59:20 -0400

    Commit: Creación de los esquemas `student_management`, `course_catalog` y `enrollments_general` en la base de datos `AcademicNotes`.

---
+++ scripts_sqlserver/schemas/create_schema_student_course_enrollments.sql
@@ -0,0 +1,17 @@
+-- ========================================
+-- Archivo: 01_schema_creation.sql
+-- Autor: Teófilo Correa Rojas
+-- Fecha: 17 de Agosto de 2025
+-- ========================================
+
+USE AcademicNotes;
+GO
+
+CREATE SCHEMA student_management;
+GO
+
+CREATE SCHEMA course_catalog;
+GO
+
+CREATE SCHEMA enrollments_general;
+GO
