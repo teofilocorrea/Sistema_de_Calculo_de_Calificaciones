473ac1aab1f8b6e8f4b23e63069c68ca9496b04a
Teófilo Correa Rojas <teofilocorrea@gmail.com>
2025-08-18 14:13:26 -0400

    Update create_database_academic_note.sql

--- scripts_sqlserver/structure_creation/database/create_database_academic_note.sql
+++ scripts_sqlserver/structure_creation/database/create_database_academic_note.sql
@@ -5,16 +5,16 @@
 -- Fecha: Julio 30 2025
 -- ========================================
 
-CREATE DATABASE AcademicaNotes
+CREATE DATABASE AcademicNotes
     ON PRIMARY (
-    NAME = AcademicaNotes_Data,
+    NAME = AcademicNotes_Data,
     FILENAME = '/var/opt/mssql/data/name_databse_Data.mdf',
     SIZE = 10 MB,
     MAXSIZE = 100 MB,
     FILEGROWTH = 5 MB
     )
     LOG ON (
-    NAME = AcademicaNotes_Log,
+    NAME = AcademicNotes_Log,
     FILENAME = '/var/opt/mssql/data/name_databse_Log.ldf',
     SIZE = 5 MB,
     MAXSIZE = 50 MB,
